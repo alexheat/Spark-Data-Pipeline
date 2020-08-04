a635259c93bf63ee53517c62c48467a18bb75eb9
Jude Kavalam <m>
2020-08-04 05:07:09 +0000

    made the event be more random - user id, price and type

--- runner.sh
+++ runner.sh
@@ -1,26 +1,26 @@
 while true; do
-  r=$(( $RANDOM % 10 )); 
-  echo ==========================
-  echo    r is $r
-  echo ==========================
-  if [ $r -eq 2 ] 
+  user=$(( $RANDOM % 5 +1 )); 
+  price=$(( $RANDOM % 29 +1 )); 
+  sword=$(( $RANDOM % 2 )); 
+  item="Steel Sword";
+  item_type="Sword";
+  
+  if [ $sword -eq 1 ]
   then
-	docker-compose exec mids ab -n 2 -c 1 -p project3/testdata/steel_sword_user_1.json -T 'application/json' http://localhost:5000/purchase_item/
+    item="Steel Shield"
+    item_type="Shield"
+    
+  fi
+  
+  payload="{\"user_id\":\"$user\", \"price\": $price, \"currency\": \"USD\", \"item\": \"$item\", \"item_type\": \"$item_type\"}"
+  echo $payload > 'tmp.json'
+  
+  r=$(( $RANDOM % 2 )); 
+  if [ $r -eq 1 ] 
+  then
+	 docker-compose exec mids ab -n 2 -c 1 -p project3/tmp.json -T 'application/json' http://localhost:5000/purchase_item/
   else
-    if [ $r -eq 4 ]
-    then
-      docker-compose exec mids ab -n 3 -c 1 -p project3/testdata/steel_sword_user_1.json -T 'application/json' http://localhost:5000/sell_item/
-    else
-      if [ $r -eq 6 ]
-      then
-        docker-compose exec mids ab -n 1 -c 1 -p project3/testdata/steel_sword_user_2.json -T 'application/json' http://localhost:5000/purchase_item/ 
-      else
-        if [ $r -eq 8 ]
-        then
-          docker-compose exec mids ab -n 3 -c 1 -p project3/testdata/steel_sword_user_2.json -T 'application/json' http://localhost:5000/sell_item/
-        fi
-      fi
-    fi
+ 	 docker-compose exec mids ab -n 2 -c 1 -p project3/tmp.json -T 'application/json' http://localhost:5000/purchase_item/
   fi
   sleep 2
 done 
